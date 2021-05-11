fb45a7dd901162edcdcf32f404940f517aa94e84
Ярослав Артюх <yaroslavartyukh@gmail.com>
2021-05-11 23:01:27 +0300

    initiator. add log4js, fix checkConfirmations

--- initiator/src/db.js
+++ initiator/src/db.js
@@ -1,40 +1,45 @@
+var log4js = require('log4js');
 const { Pool } = require("pg");
 const chainConfigDatabase = process.env.CHAINLINK_CONFIG_DATABASE;
 const supportedChainsDatabase = process.env.SUPPORTED_CHAINS_DATABASE;
 const submissionsDatabase = process.env.SUBMISSIONS_DATABASE;
 const aggregatorDatabase = process.env.AGGREGATOR_DATABASE;
+var log = log4js.getLogger("Db");
 
 class Db {
-  constructor() {
-    this.pool = new Pool();
-    this.pgClient;
-  }
+    constructor() {
+        this.pool = new Pool();
+        this.pgClient;
+    }
 
-  async connectDb() {
-    this.pgClient = await this.pool.connect();
-  }
+    async connectDb() {
+        log.info('connectDb');
+        this.pgClient = await this.pool.connect();
+    }
 
-  async createTables() {
-    // await this.pgClient.query(`drop table if exists ${submissionsDatabase} ;`);
-    // await this.pgClient.query(
-    //   `drop table if exists ${supportedChainsDatabase} ;`
-    // );
-    // await this.pgClient.query(`drop table if exists ${chainConfigDatabase} ;`);
-  }
+    async createTables() {
+        log.info('createTables');
+        // await this.pgClient.query(`drop table if exists ${submissionsDatabase} ;`);
+        // await this.pgClient.query(
+        //   `drop table if exists ${supportedChainsDatabase} ;`
+        // );
+        // await this.pgClient.query(`drop table if exists ${chainConfigDatabase} ;`);
+    }
 
-  async createChainConfig(
-    chainId,
-    cookie,
-    eiChainlinkurl,
-    eiIcAccesskey,
-    eiIcSecret,
-    eiCiAccesskey,
-    eiCiSecret,
-    mintJobId,
-    burntJobId,
-    network
-  ) {
-    await this.pgClient.query(`insert into ${chainConfigDatabase} (
+    async createChainConfig(
+        chainId,
+        cookie,
+        eiChainlinkurl,
+        eiIcAccesskey,
+        eiIcSecret,
+        eiCiAccesskey,
+        eiCiSecret,
+        mintJobId,
+        burntJobId,
+        network
+    ) {
+        log.info(`createChainConfig chainId: ${chainId}; cookie: ${cookie}; network: ${network}`);
+        await this.pgClient.query(`insert into ${chainConfigDatabase} (
           chainId,
           cookie,
           eiChainlinkurl,
@@ -57,17 +62,18 @@ class Db {
           '${burntJobId}',
           '${network}'
         ) on conflict do nothing;`);
-  }
+    }
 
-  async createSupportedChain(
-    chainId,
-    latestBlock,
-    network,
-    provider,
-    debridgeAddr,
-    interval
-  ) {
-    await this.pgClient.query(`insert into ${supportedChainsDatabase} (
+    async createSupportedChain(
+        chainId,
+        latestBlock,
+        network,
+        provider,
+        debridgeAddr,
+        interval
+    ) {
+        log.info(`createSupportedChain chainId: ${chainId}; latestBlock: ${latestBlock}; network: ${network}; provider: ${provider}; debridgeAddr: ${debridgeAddr}; interval: ${interval};`);
+        await this.pgClient.query(`insert into ${supportedChainsDatabase} (
           chainId,
           latestBlock,
           network,
@@ -82,20 +88,21 @@ class Db {
           '${debridgeAddr}',
           '${interval}'
         ) on conflict do nothing;`);
-  }
+    }
 
-  async createSubmission(
-    submissionId,
-    txHash,
-    runId,
-    chainFrom,
-    chainTo,
-    debridgeId,
-    receiverAddr,
-    amount,
-    status
-  ) {
-    await this.pgClient.query(`insert into ${submissionsDatabase} (
+    async createSubmission(
+        submissionId,
+        txHash,
+        runId,
+        chainFrom,
+        chainTo,
+        debridgeId,
+        receiverAddr,
+        amount,
+        status
+    ) {
+        log.info(`createSubmission submissionId ${submissionId}; txHash: ${txHash}; runId: ${runId}`);
+        await this.pgClient.query(`insert into ${submissionsDatabase} (
           submissionId,
           txHash,
           runId,
@@ -116,73 +123,91 @@ class Db {
           ${amount},
           ${status}
         ) on conflict do nothing;`);
-  }
+    }
 
-  async getChainConfigs() {
-    const result = await this.pgClient.query(
-      `select * from ${chainConfigDatabase};`
-    );
-    return result.rows;
-  }
+    async getChainConfigs() {
+        const result = await this.pgClient.query(
+            `select * from ${chainConfigDatabase};`
+        );
+        return result.rows;
+    }
 
-  async getChainConfig(chainId) {
-    const result = await this.pgClient.query(
-      `select * from ${chainConfigDatabase} where chainId = ${chainId};`
-    );
-    return result.rows.length > 0 ? result.rows[0] : null;
-  }
+    async getChainConfig(chainId) {
+        const result = await this.pgClient.query(
+            `select * from ${chainConfigDatabase} where chainId = ${chainId};`
+        );
+        return result.rows.length > 0 ? result.rows[0] : null;
+    }
 
-  async getAggregatorConfig(chainId) {
-    const result = await this.pgClient.query(
-      `select * from ${aggregatorDatabase} where chainTo = ${chainId};`
-    );
-    return result.rows.length > 0 ? result.rows[0] : null;
-  }
+    async getAggregatorConfig(chainId) {
+        const result = await this.pgClient.query(
+            `select * from ${aggregatorDatabase} where chainTo = ${chainId};`
+        );
+        return result.rows.length > 0 ? result.rows[0] : null;
+    }
 
-  async getSupportedChains() {
-    const result = await this.pgClient.query(
-      `select * from ${supportedChainsDatabase};`
-    );
-    return result.rows;
-  }
+    async getChainToForAggregator(aggregatorchain) {
+        const result = await this.pgClient.query(
+            `select * from ${aggregatorDatabase} where aggregatorchain = ${aggregatorchain};`
+        );
+        return result.rows;
+    }
+    async getSupportedChains() {
+        const result = await this.pgClient.query(
+            `select * from ${supportedChainsDatabase};`
+        );
+        return result.rows;
+    }
 
-  async getSubmissionsByStatus(status) {
-    const result = await this.pgClient.query(
-      `select * from ${submissionsDatabase} where status = ${status};`
-    );
-    return result.rows;
-  }
+    async getSubmissionsByStatus(status) {
+        const result = await this.pgClient.query(
+            `select * from ${submissionsDatabase} where status = ${status};`
+        );
+        return result.rows;
+    }
 
-  async getSubmission(submissionId) {
-    const result = await this.pgClient
-      .query(`select * from ${submissionsDatabase} 
+    async getSubmissionsByStatusAndChainTo(status, chainsTo) {
+        //log.debug(`select * from ${submissionsDatabase} where status = ${status} and chainto in (${chainsTo});`);
+        const result = await this.pgClient.query(
+            `select * from ${submissionsDatabase} where status = ${status} and chainto in (${chainsTo});`
+        );
+        return result.rows;
+    }
+
+    async getSubmission(submissionId) {
+        const result = await this.pgClient
+            .query(`select * from ${submissionsDatabase} 
         where submissionId = '${submissionId}';`);
-    return result.rows.length > 0 ? result.rows[0] : null;
-  }
+        return result.rows.length > 0 ? result.rows[0] : null;
+    }
 
-  async updateSupportedChainBlock(chainId, latestBlock) {
-    await this.pgClient.query(`update ${supportedChainsDatabase} set 
+    async updateSupportedChainBlock(chainId, latestBlock) {
+        log.info(`updateSupportedChainBlock chainId: ${chainId}; latestBlock: ${latestBlock}`);
+        await this.pgClient.query(`update ${supportedChainsDatabase} set 
         latestBlock = ${latestBlock}
         where chainId = ${chainId};`);
-  }
+    }
 
-  async updateSubmissionStatus(submissionId, status) {
-    await this.pgClient.query(`update ${submissionsDatabase} set 
+    async updateSubmissionStatus(submissionId, status) {
+        log.info(`updateSubmissionStatus submissionId: ${submissionId}; status: ${status}`);
+        await this.pgClient.query(`update ${submissionsDatabase} set 
         status = ${status}
         where submissionId = '${submissionId}';`);
-  }
+    }
 
-  async updateSubmissionTxHash(submissionId, txHash) {
-    await this.pgClient.query(`update ${submissionsDatabase} set 
+    async updateSubmissionTxHash(submissionId, txHash) {
+        log.info(`updateSubmissionTxHash submissionId: ${submissionId}; txHash: ${txHash}`);
+        await this.pgClient.query(`update ${submissionsDatabase} set 
         txHash = ${txHash}
         where submissionId = '${submissionId}';`);
-  }
+    }
 
-  async updateChainConfigCokie(chainId, cookie) {
-    await this.pgClient.query(`update ${chainConfigDatabase} set 
+    async updateChainConfigCokie(chainId, cookie) {
+        log.info(`updateChainConfigCokie chainId: ${chainId}; cookie: ${cookie}`);
+        await this.pgClient.query(`update ${chainConfigDatabase} set 
         cookie = '${cookie}'
         where chainId = ${chainId};`);
-  }
+    }
 }
 
 module.exports.Db = Db;
--- initiator/src/subscriber.js
+++ initiator/src/subscriber.js
@@ -1,169 +1,212 @@
+var log4js = require('log4js');
 const Web3 = require("web3");
 const whiteDebridgeAbi = require("../assets/WhiteFullDebridge.json").abi;
 const { Db } = require("./db");
 const { Chainlink } = require("./chainlink");
 const minConfirmations = process.env.MIN_CONFIRMATIONS;
 const SubmisionStatus = {
-  CREATED: 0,
-  BROADCASTED: 1,
-  CONFIRMED: 2,
-  REVERTED: 3,
+    CREATED: 0,
+    BROADCASTED: 1,
+    CONFIRMED: 2,
+    REVERTED: 3,
 };
 
+var log = log4js.getLogger("subscriber");
+
+
 class Subscriber {
-  constructor() {
-    this.db = new Db();
-    this.chainlink = new Chainlink();
-  }
-
-  async init() {
-    await this.db.connectDb();
-    await this.db.createTables();
-    await this.setAllChainlinkCookies();
-    await this.subscribe();
-  }
-
-  /* call the chainlink node and run a job */
-  async subscribe() {
-    const supportedChains = await this.db.getSupportedChains();
-    for (let supportedChain of supportedChains) {
-      const web3 = new Web3(supportedChain.provider);
-      const registerInstance = new web3.eth.Contract(
-        whiteDebridgeAbi,
-        supportedChain.debridgeaddr
-      );
-
-      setInterval(() => {
-        this.checkNewEvents(supportedChain, web3, registerInstance);
-      }, supportedChain.interval);
+    constructor() {
+        this.db = new Db();
+        this.chainlink = new Chainlink();
     }
-    const chainConfigs = await this.db.getChainConfigs();
-    for (let chainConfig of chainConfigs) {
-      setInterval(() => {
-        this.checkConfirmations(chainConfig);
-      }, 10000);
+
+    async init() {
+        log.info('init');
+        await this.db.connectDb();
+        await this.db.createTables();
+        await this.setAllChainlinkCookies();
+        await this.subscribe();
     }
-    setInterval(() => {
-      this.setAllChainlinkCookies();
-    }, 86400);
-  }
 
-  /* collect new events */
-  async checkNewEvents(supportedChain, web3, registerInstance) {
-    /* get blocks range */
-    console.log(await web3.eth.getBlockNumber());
-    const toBlock = (await web3.eth.getBlockNumber()) - minConfirmations;
-    const fromBlock = supportedChain.latestblock;
-    if (fromBlock >= toBlock) return;
+    /* call the chainlink node and run a job */
+    async subscribe() {
+        const supportedChains = await this.db.getSupportedChains();
+        for (let supportedChain of supportedChains) {
+            //const web3 = new Web3(supportedChain.provider);
+            //const registerInstance = new web3.eth.Contract(
+            //    whiteDebridgeAbi,
+            //    supportedChain.debridgeaddr
+            //);
 
-    /* get events */
-    registerInstance.getPastEvents(
-      "Sent",
-      { fromBlock, toBlock },
-      async (error, events) => {
-        await this.processNewTransfers(events, supportedChain.chainid);
-      }
-    );
-    registerInstance.getPastEvents(
-      "Burnt",
-      { fromBlock, toBlock },
-      async (error, events) => {
-        await this.processNewTransfers(events, supportedChain.chainid);
-      }
-    );
+            log.info(`setInterval ${supportedChain.interval} for checkNewEvents ${supportedChain.network}`);
+            setInterval(async () => {
+                try {
+                    await this.checkNewEvents(supportedChain);
+                }
+                catch (e) {
+                    log.error(e);
+                }
+            }, supportedChain.interval);
+        }
+        const chainConfigs = await this.db.getChainConfigs();
+        for (let chainConfig of chainConfigs) {
+            log.info(`setInterval 30000 for checkConfirmations ${chainConfig.network}`);
+            setInterval(async() => {
+                try {
+                    await this.checkConfirmations(chainConfig);
+                }
+                catch (e) {
+                    log.error(e);
+                }
+            }, 30000);
+        }
 
-    /* update lattest viewed block */
-    supportedChain.latestblock = toBlock;
-    await this.db.updateSupportedChainBlock(supportedChain.chainid, toBlock);
-  }
-
-  /* proccess new events */
-  async processNewTransfers(events, chainIdFrom) {
-    if (!events) return;
-
-    for (let e of events) {
-      /* remove chainIdTo  selector */
-      const chainIdTo = e.returnValues.chainIdTo;
-      const aggregatorInfo = await this.db.getAggregatorConfig(chainIdTo);
-      if (!aggregatorInfo) continue;
-      const chainConfig = await this.db.getChainConfig(
-        aggregatorInfo.aggregatorchain
-      );
-      if (!chainConfig) continue;
-
-      /* call chainlink node */
-      const submissionId = e.returnValues.submissionId;
-      const submission = await this.db.getSubmission(submissionId);
-      if (submission) continue;
-      this.callChainlinkNode(
-        chainConfig.submitjobid,
-        chainConfig,
-        submissionId,
-        e.returnValues,
-        chainIdFrom
-      );
+        log.info(`setInterval 86400 for setAllChainlinkCookies`);
+        setInterval(async () => {
+            try {
+                await this.setAllChainlinkCookies();
+            }
+            catch (e) {
+                log.error(e);
+            }
+        }, 86400);
     }
-  }
 
-  /* set chainlink cookies */
-  async checkConfirmations(chainConfig) {
-    const createdSubmissions = await this.db.getSubmissionsByStatus(
-      SubmisionStatus.CREATED
-    );
-    for (let submission of createdSubmissions) {
-      const runInfo = await this.chainlink.getChainlinkRun(
-        chainConfig.eichainlinkurl,
-        submission.runid,
-        chainConfig.cookie
-      );
-      if (runInfo) {
-        if (runInfo.status == "completed")
-          await this.db.updateSubmissionStatus(
-            submission.submissionId,
-            SubmisionStatus.CONFIRMED
-          );
-        if (runInfo.status == "errored")
-          await this.db.updateSubmissionStatus(
-            submission.submissionId,
-            SubmisionStatus.REVERTED
-          );
-      }
+    /* collect new events */
+    async checkNewEvents(supportedChain) {
+        log.debug(`checkNewEvents ${supportedChain.network}`);
+
+        const web3 = new Web3(supportedChain.provider);
+        const registerInstance = new web3.eth.Contract(
+            whiteDebridgeAbi,
+            supportedChain.debridgeaddr
+        );
+        /* get blocks range */
+        //console.log(await web3.eth.getBlockNumber());
+        const toBlock = (await web3.eth.getBlockNumber()) - minConfirmations;
+        const fromBlock = supportedChain.latestblock;
+        if (fromBlock >= toBlock) return;
+        log.info(`checkNewEvents ${supportedChain.network} ${fromBlock}-${toBlock}`);
+
+        /* get events */
+        registerInstance.getPastEvents(
+            "Sent",
+            { fromBlock, toBlock },
+            async (error, events) => {
+                await this.processNewTransfers(events, supportedChain.chainid);
+            }
+        );
+        registerInstance.getPastEvents(
+            "Burnt",
+            { fromBlock, toBlock },
+            async (error, events) => {
+                await this.processNewTransfers(events, supportedChain.chainid);
+            }
+        );
+
+        /* update lattest viewed block */
+        supportedChain.latestblock = toBlock;
+        await this.db.updateSupportedChainBlock(supportedChain.chainid, toBlock);
     }
-  }
 
-  /* call the chainlink node and run a job */
-  async callChainlinkNode(jobId, chainConfig, submissionId, e, chainIdFrom) {
-    const runId = await this.chainlink.postChainlinkRun(
-      jobId,
-      submissionId,
-      chainConfig.eichainlinkurl,
-      chainConfig.eiicaccesskey,
-      chainConfig.eiicsecret
-    );
+    /* proccess new events */
+    async processNewTransfers(events, chainIdFrom) {
+        if (!events) return;
 
-    await this.db.createSubmission(
-      submissionId,
-      "NULL",
-      runId,
-      chainIdFrom,
-      e.chainIdTo,
-      e.debridgeId,
-      e.receiver,
-      e.amount,
-      SubmisionStatus.CREATED
-    );
-  }
+        for (let e of events) {
+            log.info(`processNewTransfers chainIdFrom ${chainIdFrom}; submissionId: ${e.returnValues.submissionId}`);
+            log.debug(e);
+            /* remove chainIdTo  selector */
+            const chainIdTo = e.returnValues.chainIdTo;
+            const aggregatorInfo = await this.db.getAggregatorConfig(chainIdTo);
+            if (!aggregatorInfo) continue;
+            const chainConfig = await this.db.getChainConfig(
+                aggregatorInfo.aggregatorchain
+            );
+            if (!chainConfig) continue;
 
-  /* set chainlink cookies */
-  async setAllChainlinkCookies() {
-    const chainConfigs = await this.db.getChainConfigs();
-    for (const chainConfig of chainConfigs) {
-      const cookies = await this.chainlink.getChainlinkCookies(
-        chainConfig.eichainlinkurl
-      );
-      await this.db.updateChainConfigCokie(chainConfig.chainid, cookies);
+            /* call chainlink node */
+            const submissionId = e.returnValues.submissionId;
+            const submission = await this.db.getSubmission(submissionId);
+            if (submission) continue;
+            this.callChainlinkNode(
+                chainConfig.submitjobid,
+                chainConfig,
+                submissionId,
+                e.returnValues,
+                chainIdFrom
+            );
+        }
     }
-  }
+
+    /* set chainlink cookies */
+    async checkConfirmations(chainConfig) {
+        //log.debug(`checkConfirmations ${chainConfig.network}`);
+
+        //get chainTo for current aggregator
+        const supportedChains = await this.db.getChainToForAggregator(chainConfig.chainid);
+        log.debug(`checkConfirmations ${chainConfig.network} check submissions to network ${supportedChains.map(a => a.chainto)}`);
+        const createdSubmissions = await this.db.getSubmissionsByStatusAndChainTo(
+            SubmisionStatus.CREATED,
+            supportedChains.map(a => a.chainto)
+        );
+        for (let submission of createdSubmissions) {
+            const runInfo = await this.chainlink.getChainlinkRun(
+                chainConfig.eichainlinkurl,
+                submission.runid,
+                chainConfig.cookie
+            );
+            if (runInfo) {
+                if (runInfo.status == "completed")
+                    await this.db.updateSubmissionStatus(
+                        submission.submissionid,
+                        SubmisionStatus.CONFIRMED
+                    );
+                if (runInfo.status == "errored")
+                    await this.db.updateSubmissionStatus(
+                        submission.submissionid,
+                        SubmisionStatus.REVERTED
+                    );
+            }
+        }
+    }
+
+    /* call the chainlink node and run a job */
+    async callChainlinkNode(jobId, chainConfig, submissionId, e, chainIdFrom) {
+        log.info(`callChainlinkNode jobId ${jobId}; submissionId: ${submissionId}`);
+        const runId = await this.chainlink.postChainlinkRun(
+            jobId,
+            submissionId,
+            chainConfig.eichainlinkurl,
+            chainConfig.eiicaccesskey,
+            chainConfig.eiicsecret
+        );
+        log.info(`Received runId ${runId}; submissionId: ${submissionId}`);
+        await this.db.createSubmission(
+            submissionId,
+            "NULL",
+            runId,
+            chainIdFrom,
+            e.chainIdTo,
+            e.debridgeId,
+            e.receiver,
+            e.amount,
+            SubmisionStatus.CREATED
+        );
+    }
+
+    /* set chainlink cookies */
+    async setAllChainlinkCookies() {
+        log.debug(`Start setAllChainlinkCookies`);
+        const chainConfigs = await this.db.getChainConfigs();
+        for (const chainConfig of chainConfigs) {
+            log.debug(`setAllChainlinkCookies ${chainConfig.network}`);
+            const cookies = await this.chainlink.getChainlinkCookies(
+                chainConfig.eichainlinkurl
+            );
+            await this.db.updateChainConfigCokie(chainConfig.chainid, cookies);
+        }
+    }
 }
 
 module.exports.Subscriber = Subscriber;
