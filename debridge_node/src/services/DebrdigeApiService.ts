83eb0a33c2100191728847155a14c99baecc2daa
d1r1 <61736932+d1r1@users.noreply.github.com>
2021-12-08 22:46:36 +0300

    Release/v1.1.2 (#73)
    * mount configs dir as volume (#63)
    
    Co-authored-by: d1r1 <me@d1r1.me>
    
    * add checker for chains config (authored by kudryaA) (#64)
    
    Co-authored-by: d1r1 <me@d1r1.me>
    
    * mount configs to built dist (#65)
    
    Co-authored-by: d1r1 <me@d1r1.me>
    
    * change version v1.1.1 to v1.1.2
    
    * change build ipfs-daemon image to pull it from public registry
    
    * Send  node version
    
    * Send  node version with interval
    
    * fix path to the main for debridge-node (#71)
    
    Co-authored-by: d1r1 <me@d1r1.me>
    
    * fix debridge-node config path and add check for valid config chainId (#72)
    
    Co-authored-by: d1r1 <me@d1r1.me>
    
    * fix typo
    
    Co-authored-by: d1r1 <me@d1r1.me>
    Co-authored-by: Anton <kam123ua@gmail.com>

--- debridge_node/src/services/DebrdigeApiService.ts
+++ debridge_node/src/services/DebrdigeApiService.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { SubmissionEntity } from 'src/entities/SubmissionEntity';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
@@ -14,9 +14,11 @@ import { ProgressInfoDTO, ValidationProgressDTO } from '../dto/debridge_api/Vali
 import { createProxy } from '../utils/create.proxy';
 import { UpdateOrbirDbDTO } from '../dto/debridge_api/UpdateOrbirDbDTO';
 import { HttpAuthService } from './HttpAuthService';
+import { version } from './../../package.json';
 
 @Injectable()
-export class DebrdigeApiService extends HttpAuthService {
+export class DebrdigeApiService extends HttpAuthService implements OnModuleInit {
+  private readonly updateVersionInterval = 60000; //1m
   private account: Account;
   private web3: Web3;
 
@@ -24,6 +26,18 @@ export class DebrdigeApiService extends HttpAuthService {
     super(httpService, new Logger(DebrdigeApiService.name), configService.get('API_BASE_URL'), '/Account/authenticate');
     this.web3 = createProxy(new Web3(), { logger: this.logger });
     this.account = this.web3.eth.accounts.decrypt(JSON.parse(readFileSync('./keystore.json', 'utf-8')), process.env.KEYSTORE_PASSWORD);
+  }
+
+  async onModuleInit() {
+    const updateVersionInterval = setInterval(async () => {
+      try {
+        await this.updateVersion(version);
+        this.logger.log(`Sending event to update node version is finished`);
+        clearInterval(updateVersionInterval);
+      } catch (e) {
+        this.logger.warn(`Error in sending event to update node version`);
+      }
+    }, this.updateVersionInterval);
   }
 
   private getLoginDto() {
@@ -39,9 +53,16 @@ export class DebrdigeApiService extends HttpAuthService {
   async updateOrbitDb(requestBody: UpdateOrbirDbDTO) {
     this.logger.log(`updateOrbitDb ${requestBody} is started`);
     const httpResult = await this.authRequest('/Validator/updateOrbitDb', requestBody, this.getLoginDto());
-
     this.logger.verbose(`response: ${httpResult.data}`);
     this.logger.log(`updateOrbitDb is finished`);
+  }
+
+  async updateVersion(version: string) {
+    this.logger.log(`updateVersion ${version} is started`);
+    const httpResult = await this.authRequest('/Validator/setNodeVersion', { version }, this.getLoginDto());
+
+    this.logger.verbose(`response: ${httpResult.data}`);
+    this.logger.log(`updateVersion is finished`);
   }
 
   async uploadToApi(submissions: SubmissionEntity[]): Promise<SubmissionConfirmationResponse[]> {
