f941f924fe6709b0b5b1856aa5e25ab5d9d7e1bb
mncdg <mncdg@ukr.net>
2022-02-13 03:06:42 +0200

    Fix

--- debridge_node/src/subscribe/actions/AddNewEventsAction.ts
+++ debridge_node/src/subscribe/actions/AddNewEventsAction.ts
@@ -141,7 +141,6 @@ export class AddNewEventsAction {
    * @param {number} to
    */
   async process(chainId: number, from: number = undefined, to: number = undefined) {
-    this.logger = new Logger(`${AddNewEventsAction.name} ${chainId}`);
     this.logger.verbose(`checkNewEvents ${chainId}`);
     const supportedChain = await this.supportedChainRepository.findOne({
       where: {
