f5fbdad0a487729a7a18842b99d24fc738ab1655
mncdg <mncdg@ukr.net>
2022-03-18 02:08:34 +0200

    Web3 keepAlive false

--- debridge_node/src/services/Web3Service.ts
+++ debridge_node/src/services/Web3Service.ts
@@ -13,7 +13,6 @@ export class Web3Custom extends Web3 {
 export class Web3Service {
   private readonly logger = new Logger(Web3Service.name);
   private readonly web3Timeout: number;
-  private readonly providersMap = new Map<string, Web3Custom>();
 
   constructor(private readonly configService: ConfigService) {
     this.web3Timeout = parseInt(configService.get('WEB3_TIMEOUT', '10000'));
@@ -25,19 +24,9 @@ export class Web3Service {
 
   async web3HttpProvider(chainProvider: ChainProvider): Promise<Web3Custom> {
     for (const provider of [...chainProvider.getNotFailedProviders(), ...chainProvider.getFailedProviders()]) {
-      if (this.providersMap.has(provider)) {
-        const web3 = this.providersMap.get(provider);
-        const isWorking = await this.checkConnectionHttpProvider(web3);
-        if (isWorking) {
-          this.logger.verbose(`Old provider is working`);
-          return web3;
-        }
-        this.logger.error(`Old provider ${provider} is not working`);
-      }
-
       const httpProvider = new Web3Custom.providers.HttpProvider(provider, {
         timeout: this.web3Timeout,
-        keepAlive: true,
+        keepAlive: false,
         headers: chainProvider.getChainAuth(provider),
       });
 
@@ -52,7 +41,6 @@ export class Web3Service {
         await this.validateChainId(chainProvider, provider);
       }
       chainProvider.setProviderStatus(provider, true);
-      this.providersMap.set(provider, web3);
       return web3;
     }
     this.logger.error(`Cann't connect to any provider ${chainProvider.getAllProviders()}`);
