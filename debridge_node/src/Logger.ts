783c9827c81bc43811945ee1f75ef27a7c055bf0
mncdg <mncdg@ukr.net>
2022-04-03 20:44:09 +0300

    Sending logs to logstash

--- debridge_node/src/Logger.ts
+++ debridge_node/src/Logger.ts
@@ -1,11 +1,38 @@
 import { ConsoleLogger } from '@nestjs/common';
 import * as Sentry from '@sentry/minimal';
+import * as gelfLog from 'gelf-pro';
 
 export class Logger extends ConsoleLogger {
+  constructor() {
+    super();
+    gelfLog.setConfig({ adapterOptions: { host: process.env.GELF_HOST, port: parseInt(process.env.GELF_PORT) } });
+  }
+
   error(message: any, stack?: string) {
     if (process.env.SENTRY_DSN) {
       Sentry.captureMessage(`[${stack}] ${message}`);
     }
+    gelfLog.message(`[${stack}] ${message}`, 3);
     super.error(`[${stack}] ${message}`);
+  }
+
+  warn(message: any, stack?: string) {
+    gelfLog.message(`[${stack}] ${message}`, 4);
+    super.warn(`[${stack}] ${message}`);
+  }
+
+  verbose(message: any, stack?: string) {
+    gelfLog.message(`[${stack}] ${message}`, 7);
+    super.verbose(`[${stack}] ${message}`);
+  }
+
+  debug(message: any, stack?: string) {
+    gelfLog.message(`[${stack}] ${message}`, 7);
+    super.debug(`[${stack}] ${message}`);
+  }
+
+  info(message: any, stack?: string) {
+    gelfLog.message(`[${stack}] ${message}`, 6);
+    super.log(`[${stack}] ${message}`);
   }
 }
